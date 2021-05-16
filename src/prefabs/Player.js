aa03535fab83998e2106092b0ea24202bb403b4f
Juan Alvarado <juralvar@ucsc.com>
2021-05-16 11:42:48 -0700

    Added movement to player.js

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -10,7 +10,30 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
 
     update() {
-
+      // player's 8 direction movement
+      if(keyLEFT.isDown){
+        this.player.body.setVelocityX(-100);                
+      }
+      else if(keyRIGHT.isDown){
+         this.player.body.setVelocityX(100);
+      }
+      else{
+         this.player.body.setVelocityX(0);
+      }
+      if(keyUP.isDown){
+          this.player.body.setVelocityY(-100);                
+      }
+      else if(keyDOWN.isDown){
+          this.player.body.setVelocityY(100);
+      }
+      else{
+          this.player.body.setVelocityY(0);
+      }
+      // If no movement keys are pressed character will stop moving
+      if(!keyLEFT.isDown && !keyRIGHT.isDown && !keyUP.isDown && !keyDOWN.isDown){
+         this.player.body.setVelocityY(0);
+         this.player.body.setVelocityX(0);
+      }
     }
 
 }
