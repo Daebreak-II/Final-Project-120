fad70108fbdfc196459b2506248d0f86405f7f47
Juan Alvarado <juralvar@ucsc.com>
2021-06-02 23:12:11 -0700

    added menu screen, need to ask about title

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,48 +1,63 @@
 class Menu extends Phaser.Scene {
-    constructor() {
-      super("menuScene");
+  constructor() {
+    super("menuScene");
+  }
+
+  preload(){
+    this.load.image('menu', './Assets/sprites/mainMenu.png');
+    this.load.image('play', './Assets/sprites/startButton.png');
+    this.load.image('quit', './Assets/sprites/quitButton.png');
+    this.load.image('options', './Assets/sprites/optionsButton.png');
+  }
+  
+  create() {
+      //setting up main meny screen images
+      this.menuImage = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'menu').setOrigin(0, 0);
+
+      //setting up options in menu
+      this.selectPlay = this.add.sprite(300, game.config.height/2 - 150, 'play').setOrigin(0, 0);
+      this.selectPlay.setScale(1.5);
+
+      this.mousePlay = this.selectPlay.setInteractive();
+
+      this.selectOptions = this.add.image(300, game.config.height/2, 'options').setOrigin(0, 0);
+      this.selectOptions.setScale(1.5);
+
+
+      this.mouseOptions = this.selectOptions.setInteractive();
+      
+      this.mousePointerisOverPlay = false;
+
+      this.mousePointerisOverOptions = false;
+
+      //setting up mouse cursor over object
+      this.mousePlay.on('pointerover', () => { 
+        this.mousePointerisOverPlay = true;
+        
+      });
+      
+      this.mousePlay.on('pointerout', () => { 
+        this.mousePointerisOverPlay = false; 
+      });
+
+      this.mouseOptions.on('pointerover', () => { 
+        this.mousePointerisOverOptions = true;
+        
+      });
+      
+      this.mouseOptions.on('pointerout', () => { 
+        this.mousePointerisOverOptions = false; 
+      });
+      
+  }
+  update(){
+    
+    if(game.input.activePointer.leftButtonDown() && this.mousePointerisOverPlay){
+      this.scene.start("playScene");      
     }
 
-    preload() {
-
+    if(game.input.activePointer.leftButtonDown() && this.mousePointerisOverOptions){
+      this.scene.start("optionScene");
     }
-
-    create() {
-      //this.scene.start('playScene');
-      let menuConfig = {
-        fontFamily: 'Courier',
-        fontSize: '48px',
-        color: '#FFFFFF',
-        align: 'right',
-        padding: {
-            top: 5,
-            bottom: 5,
-        },
-        Width: 0
-      }
-
-      this.add.text(game.config.width/2, game.config.height/2, 'Trail Behind', menuConfig).setOrigin(0.5,0);
-      let menuConfig2 = {
-        fontFamily: 'Courier',
-        fontSize: '24px',
-        color: '#FFFFFF',
-        align: 'right',
-        padding: {
-            top: 5,
-            bottom: 5,
-        },
-        Width: 0
-      }
-      this.add.text(game.config.width/2, game.config.height/2 + 100, 'Press the Space Bar to start', menuConfig).setOrigin(0.5,0);
-
-      keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-    }
-
-    update() {
-      if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
-        // easy mode
-        this.scene.start('playScene');    
-      }
-    }
-
+  }
 }
